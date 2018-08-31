564134e82eaa6c4a83dbf14165c73b55cf7063f2
Stamates <Stamates@users.noreply.github.com>
2018-08-31 11:03:36 -0400

    Coveralls cleanup

--- apps/block_scout_web/test/block_scout_web/features/pages/chain_page.ex
+++ apps/block_scout_web/test/block_scout_web/features/pages/chain_page.ex
@@ -6,7 +6,6 @@ defmodule BlockScoutWeb.ChainPage do
   import Wallaby.Query, only: [css: 1, css: 2]
 
   alias Explorer.Chain.Transaction
-  alias BlockScoutWeb.ChainView
 
   def blocks(count: count) do
     css("[data-selector='chain-block']", count: count)
@@ -14,14 +13,6 @@ defmodule BlockScoutWeb.ChainPage do
 
   def contract_creation(%Transaction{created_contract_address_hash: hash}) do
     css("[data-test='contract-creation'] [data-address-hash='#{hash}']")
-  end
-
-  def exchange_rate(token) do
-    css("[data-selector='exchange-rate']", text: ChainView.format_exchange_rate(token))
-  end
-
-  def non_loaded_transaction_count(count) do
-    css("[data-selector='channel-batching-count']", text: count)
   end
 
   def search(session, text) do
@@ -40,14 +31,6 @@ defmodule BlockScoutWeb.ChainPage do
 
   def transactions(count: count) do
     css("[data-test='chain_transaction']", count: count)
-  end
-
-  def transaction(%Transaction{hash: transaction_hash}) do
-    css("[data-transaction-hash='#{transaction_hash}']")
-  end
-
-  def transaction_status(%Transaction{hash: transaction_hash}) do
-    css("[data-transaction-hash='#{transaction_hash}'] [data-test='transaction_status']")
   end
 
   def visit_page(session) do
--- apps/block_scout_web/test/block_scout_web/features/pages/transaction_page.ex
+++ apps/block_scout_web/test/block_scout_web/features/pages/transaction_page.ex
@@ -18,8 +18,4 @@ defmodule BlockScoutWeb.TransactionPage do
   def visit_page(session, %Transaction{hash: transaction_hash}) do
     visit(session, "/en/transactions/#{transaction_hash}")
   end
-
-  def visit_page(session, transaction_hash = %Hash{}) do
-    visit(session, "/en/transactions/#{transaction_hash}")
-  end
 end
